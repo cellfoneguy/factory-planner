39f643292addceef513c8041a2c301f52ac9b120
Alex Bai <cellfoneguy@gmail.com>
2023-07-09 00:45:58 -0500

    initial commit - practicing html/js

---
+++ planner.js
@@ -0,0 +1,21 @@
+
+function recalculateRow1() {
+  arg1 = parseInt(document.getElementById("station1").value);
+  arg2 = parseInt(document.getElementById("stationQ1").value);
+  document.getElementById("result1").innerHTML = arg1 + arg2;
+}
+
+function recalculate() {
+  for (let i=0; i<tbl.rows.length; i++){
+    
+  }
+}
+
+function addRow() {
+  tbl = document.getElementById("table1");
+
+  var row = table.insertRow(-1);
+  for (let i=0; i<3; i++) {
+    
+  }
+}
