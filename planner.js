ce2d21be5db664a6d128c48387b46e17dd6651f2
Alex Bai <cellfoneguy@gmail.com>
2023-07-09 02:57:39 -0500

    planner 3 columns. station, station quantity, and recipe

--- planner.js
+++ planner.js
@@ -1,21 +1,95 @@
 
-function recalculateRow1() {
-  arg1 = parseInt(document.getElementById("station1").value);
-  arg2 = parseInt(document.getElementById("stationQ1").value);
-  document.getElementById("result1").innerHTML = arg1 + arg2;
+// globals
+// stations
+STATIONS = [
+  "Miner",
+  "Smelter",
+  "Oil Refinery",
+]
+
+// recipes
+RECIPES = {};
+RECIPES["Miner"] = [
+  "Iron Ore",
+  "Copper Ore",
+  "Coal",
+];
+RECIPES["Smelter"] = [
+  "Iron Ingot",
+  "Copper Ingot",
+  "Energetic Graphene",
+];
+RECIPES["Oil Refinery"] = [
+  "Plasma Refining",
+]
+
+// populate the station selector dropdown
+function populateStationSelector(stationSelector) {
+  for (let i=0; i < STATIONS.length; i++) {
+    let opt = document.createElement("option");
+    opt.text = STATIONS[i];
+    stationSelector.add(opt, null);
+  }
+}
+
+// populate the recipe selector dropdown
+function populateRecipeSelector (recipeSelector) {
+  let station = recipeSelector.parentNode.parentNode.cells[0].children[0].value
+  let recipes = RECIPES[station];
+  document.getElementById("debug").innerHTML = recipes;
+  for (let i=0; i < recipes.length; i++) {
+    let opt = document.createElement("option");
+    opt.text = recipes[i];
+    recipeSelector.add(opt, null);
+  }
+}
+
+// when the station is changed, repopulate the recipe dropdown
+function repopulateRecipeSelector (evt) {
+  let stationSelector = evt.currentTarget;
+  let recipeSelector = stationSelector.parentNode.parentNode.cells[2].children[0]
+
+  // clear dropdown
+  while (recipeSelector.length > 0) {
+    recipeSelector.remove(0);
+  }
+
+  // repopulate
+  let recipes = RECIPES[stationSelector.value];
+  for (let i=0; i < recipes.length; i++) {
+    let opt = document.createElement("option");
+    opt.text = recipes[i];
+    recipeSelector.add(opt, null);
+  }
 }
 
 function recalculate() {
-  for (let i=0; i<tbl.rows.length; i++){
-    
-  }
 }
 
 function addRow() {
-  tbl = document.getElementById("table1");
+  let tbl = document.getElementById("table1");
 
-  var row = table.insertRow(-1);
-  for (let i=0; i<3; i++) {
-    
-  }
+  let row = tbl.insertRow(-1);
+
+  // add station dropdown
+  let stationCell = row.insertCell(-1);
+  let stationSelector = document.createElement("select");
+  stationSelector.setAttribute("class", "stationSelector");
+  stationCell.appendChild(stationSelector);
+  populateStationSelector(stationSelector);
+  stationSelector.addEventListener("change", repopulateRecipeSelector, false);
+
+  // add station quantity
+  let stationQuantityCell = row.insertCell(-1);
+  let stationQuantityInput = document.createElement("input");
+  stationQuantityCell.appendChild(stationQuantityInput);
+  stationQuantityInput.setAttribute("type", "text");
+
+  // add recipe
+  let recipeCell = row.insertCell(-1);
+  let recipeSelector = document.createElement("select");
+  recipeSelector.setAttribute("class", "recipeAll");
+  recipeCell.appendChild(recipeSelector);
+  populateRecipeSelector(recipeSelector);
+  recipeCell.appendChild(recipeSelector);
 }
